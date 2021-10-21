373cdce235c62426494d720c27c4a216e0b03d16
Robert Stacey <rjstacey@gmail.com>
2021-10-21 07:04:14 -0700

    storybook config

---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
