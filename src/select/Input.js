5f0a6a0464c9f58b4cef7a76943f0ff66d74471c
Robert Stacey <rjstacey@gmail.com>
2022-02-21 15:29:09 -0800

    -

--- src/select/Input.js
+++ src/select/Input.js
@@ -1,71 +1,20 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled from '@emotion/styled';
-import { LIB_NAME } from './constants';
 
-const handlePlaceHolder = (props, state) => {
-	const { addPlaceholder, searchable, placeholder } = props;
-	const noValues = state.values && state.values.length === 0;
-	const hasValues = state.values && state.values.length > 0;
-
-	if (hasValues && addPlaceholder && searchable) {
-		return addPlaceholder;
-	}
-
-	if (noValues) {
-		return placeholder;
-	}
-
-	if (hasValues && !searchable) {
-		return '';
-	}
-
-	return '';
-};
-
-function Input({props, state, methods}) {
-	const inputRef = React.useRef();
-
-	React.useEffect(() => {
-		if (!inputRef.current)
-			return;
-		if (state.isOpen)
-			inputRef.current.focus();
-		else
-			inputRef.current.blur();
-	}, [state.isOpen])
-
-	return (
-		<InputComponent
-			ref={inputRef}
-			tabIndex="-1"
-			className={`${LIB_NAME}-input`}
-			size={methods.getInputSize()}
-			value={state.search}
-			onChange={(event) => methods.setSearch(event.target.value)}
-			placeholder={handlePlaceHolder(props, state)}
-			disabled={props.disabled}
-		/>
-	);
-}
+const Input = ({inputRef, value, onChange}) => 
+	<input
+		ref={inputRef}
+		tabIndex="-1"
+		className='select-input'
+		style={{size: value.length + 1}}
+		value={value}
+		onChange={(event) => onChange(event.target.value)}
+	/>
 
 Input.propTypes = {
-	props: PropTypes.object,
-	state: PropTypes.object,
-	methods: PropTypes.object
+	inputRef: PropTypes.object.isRequired,
+	value: PropTypes.string.isRequired,
+	onChange: PropTypes.func.isRequired,
 };
-
-const InputComponent = styled.input`
-	line-height: inherit;
-	border: none;
-	margin-left: 5px;
-	background: transparent;
-	padding: 0;
-	width: calc(${({ size }) => `${size}ch`} + 5px);
-	font-size: smaller;
-	:focus {
-		outline: none;
-	}
-`;
 
 export default Input;
