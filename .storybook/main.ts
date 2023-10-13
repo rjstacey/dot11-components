1dc76903df1699b35a31d486d36d5ca272d9a171
Robert Stacey <rjstacey@gmail.com>
2023-10-13 14:56:50 -0700

    storeybook upgrade

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,21 @@
 module.exports = {
   stories: ["../src/**/stories.{js,tsx}", "../src/stories/*.stories.{js,tsx}"],
-  framework: '@storybook/react',
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app"],
-  core: {
-    builder: "webpack5"
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
+
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app"],
+
   webpackFinal: async (config, { configType }) => {
     // Make whatever fine-grained changes you need
     // Return the altered config
     config.resolve.extensions.push('.ts', '.tsx')
     return config;
   },
+
+  docs: {
+    autodocs: true
+  }
 };
