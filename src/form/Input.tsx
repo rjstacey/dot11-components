70fd98515e3d971eaabc1fbb60b46a1a6bde92fd
Robert Stacey <rjstacey@gmail.com>
2023-12-25 15:44:51 -0800

    forward ref

--- src/form/Input.tsx
+++ src/form/Input.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import styles from "./form.module.css";
 
-const Input = ({
-	className,
-	...props
-}: React.ComponentProps<"input">) => (
-	<input
-		className={styles["input"] + (className ? " " + className : "")}
-		{...props}
-	/>
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+	({ className, ...props }, ref) => (
+		<input
+			ref={ref}
+			className={styles["input"] + (className ? " " + className : "")}
+			{...props}
+		/>
+	)
 );
 
 export default Input;
