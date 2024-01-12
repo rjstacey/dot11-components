a34ee588ee65834dd96bddb5a2afe1d1607cc73e
Robert Stacey <rjstacey@gmail.com>
2024-01-12 08:02:28 -0800

    changing selector

--- src/table/DateFilter.tsx
+++ src/table/DateFilter.tsx
@@ -1,60 +1,68 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Input, Row, Field } from '../form';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Input, Row, Field } from "../form";
 import {
 	FilterComp,
 	AppTableDataSelectors,
 	AppTableDataActions,
-    CompOp
-} from '../store/appTableData';
+	CompOp,
+} from "../store/appTableData";
 
-type DateFilterProps = {
+export function DateFilter({
+	dataKey,
+	selectors,
+	actions,
+}: {
 	dataKey: string;
 	selectors: AppTableDataSelectors;
-	actions: AppTableDataActions
-};
-
-export function DateFilter({dataKey, selectors, actions}: DateFilterProps) {
-
+	actions: AppTableDataActions;
+}) {
 	const dispatch = useDispatch();
 
-	const {beforeDate, afterDate} = useSelector((state: any) => {
-        const filter = selectors.selectFilter(state, dataKey);
-        const beforeDate = filter.comps.find(c => c.operation === CompOp.LT)?.value || '';
-        const afterDate = filter.comps.find(c => c.operation === CompOp.GT)?.value || '';
-        return {beforeDate, afterDate}
-    });
+	const beforeDate = useSelector((state: any) => {
+		const filter = selectors.selectFilter(state, dataKey);
+		return filter.comps.find((c) => c.operation === CompOp.LT)?.value || "";
+	});
 
-    const setBefore = (date: string) => {
-        const comps: FilterComp[] = date? [{value: date, operation: CompOp.LT}]: [];
-		dispatch(actions.setFilter({dataKey, comps}));
-    }
+	const afterDate = useSelector((state: any) => {
+		const filter = selectors.selectFilter(state, dataKey);
+		return filter.comps.find((c) => c.operation === CompOp.GT)?.value || "";
+	});
 
-    const setAfter = (date: string) => {
-        const comps: FilterComp[] = date? [{value: date, operation: CompOp.GT}]: [];
-		dispatch(actions.setFilter({dataKey, comps}));
-    }
+	const setBefore = (date: string) => {
+		const comps: FilterComp[] = date
+			? [{ value: date, operation: CompOp.LT }]
+			: [];
+		dispatch(actions.setFilter({ dataKey, comps }));
+	};
+
+	const setAfter = (date: string) => {
+		const comps: FilterComp[] = date
+			? [{ value: date, operation: CompOp.GT }]
+			: [];
+		dispatch(actions.setFilter({ dataKey, comps }));
+	};
 
 	return (
-        <>
-            <Row>
-                <Field label='Before:'>
-                    <Input
-                        type='date'
-                        value={beforeDate}
-                        onChange={e => setBefore(e.target.value)}
-                    />
-                </Field>
-            </Row>
-            <Row>
-                <Field label='After:'>
-                    <Input
-                        type='date'
-                        value={afterDate}
-                        onChange={e => setAfter(e.target.value)}
-                    />
-                </Field>
-            </Row>
-        </>
-	)
+		<>
+			<Row>
+				<Field label="Before:">
+					<Input
+						type="date"
+						value={beforeDate}
+						onChange={(e) => setBefore(e.target.value)}
+					/>
+				</Field>
+			</Row>
+			<Row>
+				<Field label="After:">
+					<Input
+						type="date"
+						value={afterDate}
+						onChange={(e) => setAfter(e.target.value)}
+					/>
+				</Field>
+			</Row>
+		</>
+	);
 }
